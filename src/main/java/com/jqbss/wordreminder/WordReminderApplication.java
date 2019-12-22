86bb0703c2ae914e2be6fe81ba1d93b69097275c
Jakub Krasucki <jakub.krasucki@interia.pl>
2019-12-22 20:34:50 +0100

    Initial commit

---
+++ src/main/java/com/jqbss/wordreminder/WordReminderApplication.java
@@ -0,0 +1,13 @@
+package com.jqbss.wordreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordReminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jqbss/wordreminder/WordReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jqbss.wordreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
